bb88827a3e82da6b9b24389b5a45930c4bb66a14
birdy <gob@live.ru>
2015-08-03 01:52:50 +0300

    (*) partly documented (-) removed WpfAnimatedGif (*) some code cleanup

--- LightImageViewer/MainWindow.xaml.cs
+++ LightImageViewer/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
 using System.IO;
+using System.Collections;
 
 namespace LightImageViewer
 {
@@ -22,49 +23,93 @@ namespace LightImageViewer
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<string> filenames = new List<string>();
-        int currentFileIndex = 0;
+        /// <summary>
+        /// Список файлов в текущей директории
+        /// </summary>
+        private List<string> _filenames = new List<string>();
 
+        /// <summary>
+        /// Индекс картинки, отображаемой в данный момент
+        /// </summary>
+        private int _currentFileIndex = 0;
+
+        private int _timeToWait;
+        private int _timeToWaitPreset = 350;
+        private int _timeToWaitStep = 10;
+        private bool _recaching = false;
+
+        private int _minSize = 50;
+        private int _maxSizeMultiplier = 4;
+        private double _scaleFactor = 1.1;
+
+        private bool _panning = false;
+        private Point _lastPoint;
+
+        #region Конструкторы
+
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        #endregion
+
+        /// <summary>
+        /// Обработка нажатия на кнопку закрытия окна
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
         
+        /// <summary>
+        /// Обработка загрузки главного окна приложения
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            System.Windows.Forms.Screen currentScreen;
-            currentScreen = System.Windows.Forms.Screen.FromPoint(System.Windows.Forms.Cursor.Position);
+            // окно можно растянуть (так чтобы у него не было рамки и чтобы оно не закрывало панель задач)
+            // только вручную, что и сделано тут
+            var currentScreen = System.Windows.Forms.Screen.FromPoint(System.Windows.Forms.Cursor.Position);
             Left = currentScreen.WorkingArea.X;
             Top = currentScreen.WorkingArea.Y;
             Height = currentScreen.WorkingArea.Height;
             Width = currentScreen.WorkingArea.Width;
-            canvas.Background = Brushes.AliceBlue;
         }
 
+        /// <summary>
+        /// Обработка окончания рендеринга формы
+        /// </summary>
+        /// <param name="e"></param>
         protected override void OnContentRendered(EventArgs e)
         {
+            // получаем открываемую картинку и передаёт её в контрол отображения
+            // передать изображение на отрисовку можно только после того, как все контролы будут отрисованы на экране
             var dict = Environment.GetCommandLineArgs();
-
-            canvas.CurrentPath = dict[1];
-            canvas.InvalidateVisual();
-            GetFilesList();
-
             if (dict.Length == 1) return;
+            canvas.CurrentPath = dict[1];
+            GetFilesList();
         }
         
+        /// <summary>
+        /// Методы запускающийся в отдельном потоке и используемый для того, чтобы перекэшировать отображаемое изображение,
+        /// но сделать это не сразу, а с небольшой паузой. Таким образом, например при вращении колеса мыши 
+        /// перекэширование будет происходить после того, как пользователь "докрутит до нужного масштаба", а не после
+        /// каждого деления колёсика
+        /// </summary>
         public void WaitToRecache()
         {
-            recaching = true;
-            counter++;
-            while (timeToWait > 0)
+            _recaching = true;
+            while (_timeToWait > 0)
             {
-                timeToWait -= timeToWaitStep;
-                System.Threading.Thread.Sleep(timeToWaitStep);
+                _timeToWait -= _timeToWaitStep;
+                System.Threading.Thread.Sleep(_timeToWaitStep);
             }
             Application.Current.Dispatcher.BeginInvoke((Action)(() =>
             {
@@ -74,25 +119,20 @@ namespace LightImageViewer
                     canvas.UpdateImageSource();
                 }
             }));
-            counter--;
-            recaching = false;
+            _recaching = false;
         }
 
-        int counter = 0;
-        int timeToWait;
-        int timeToWaitPreset = 350;
-        int timeToWaitStep = 10;
-        bool recaching = false;
+        #region Масштабирование
 
-        private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            Scale(e.Delta, e.GetPosition(canvas));
-        }
-
+        /// <summary>
+        /// Масштабирование, используется в событиях вращения колеса мыши и нажатия стрелок 
+        /// вверх и вниз
+        /// </summary>
+        /// <param name="delta">Направление изменения масштаба</param>
+        /// <param name="scaleCenter">Точка, относительно которой масштабируем (используется для сдвига изобажения)</param>
         public void Scale(int delta, Point scaleCenter)
         {
-            if (canvas.CurrentImage == null) return;
-
+            // контролируем, было ли изменено изображение. если нет, то рекэширование не трежуется
             bool resized = false;
 
             var oldWidth = (int)canvas.CurrentImage.Width;
@@ -102,83 +142,114 @@ namespace LightImageViewer
             var checkWidth = oldWidth > oldHeight;
             if (delta < 0)
             {
-                if (checkWidth ? oldHeight > minSize : oldWidth > minSize)
+                if (checkWidth ? oldHeight > _minSize : oldWidth > _minSize)
                 {
-                    width = (int)Math.Max(DecreaseSize(oldWidth), minSize);
-                    height = (int)Math.Max(DecreaseSize(oldHeight), minSize);
+                    width = (int)Math.Max(DecreaseSize(oldWidth), _minSize);
+                    height = (int)Math.Max(DecreaseSize(oldHeight), _minSize);
                     resized = true;
                 }
             }
             else
             {
                 if (checkWidth ?
-                    oldWidth < maxSizeMultiplier * canvas.CurrentImage.Width :
-                    oldHeight < maxSizeMultiplier * canvas.CurrentImage.Height)
+                    oldWidth < _maxSizeMultiplier * canvas.CurrentImage.Width :
+                    oldHeight < _maxSizeMultiplier * canvas.CurrentImage.Height)
                 {
-                    width = (int)Math.Min(IncreaseSize(oldWidth), maxSizeMultiplier * canvas.CurrentImage.Width);
-                    height = (int)Math.Min(IncreaseSize(oldHeight), maxSizeMultiplier * canvas.CurrentImage.Height);
+                    width = (int)Math.Min(IncreaseSize(oldWidth), _maxSizeMultiplier * canvas.CurrentImage.Width);
+                    height = (int)Math.Min(IncreaseSize(oldHeight), _maxSizeMultiplier * canvas.CurrentImage.Height);
                     resized = true;
                 }
             }
 
-            if (!resized || !canvas.CanMakeBigger(width, height)) return;
+            // если изображение не изменилось, то ничего не делаем
+            if (!resized) return;
 
-            timeToWait = timeToWaitPreset;
+            // обновляем счетчик ожидания рекэширования
+            _timeToWait = _timeToWaitPreset;
+
+            // задаём новые размеры изображения
             canvas.CurrentImage.Width = width;
             canvas.CurrentImage.Height = height;
-            if (!recaching) Task.Factory.StartNew(WaitToRecache);
 
+            // если кэширование не начато, то запустить поток с ожиданием
+            if (!_recaching) Task.Factory.StartNew(WaitToRecache);
+
+            // сдвигаем изображение так, чтобы мышь осталась в той же точке на картинке (масштабирование 
+            // относительно указателя мыши)
             var mp = scaleCenter;
             var a = (oldHeight - height) * (canvas.ImgTop - mp.Y) / oldHeight;
             var b = (oldWidth - width) * (canvas.ImgLeft - mp.X) / oldWidth;
             canvas.ImgLeft -= b;
             canvas.ImgTop -= a;
-            //RectangleGeometry clipGeometry = new RectangleGeometry(new Rect(new Point(Top, Left), new Point(width, height)));
-            //canvas.CurrentImage.Clip = clipGeometry;
-            canvas.InvalidateVisual();
-        }
-
-        int minSize = 50;
-        int maxSizeMultiplier = 4;
-
-        public double DecreaseSize(double scale)
-        {
-            return scale /= 1.1;
-        }
-
-        public double IncreaseSize(double scale)
-        {
-            return scale *= 1.1;
         }
 
         /// <summary>
-        /// Width is bigger then height
+        /// Уменьшение масштаба
         /// </summary>
-        /// <param name="w"></param>
-        /// <param name="h"></param>
+        /// <param name="scale"></param>
         /// <returns></returns>
+        public double DecreaseSize(double scale)
+        {
+            return scale /= _scaleFactor;
+        }
+
+        /// <summary>
+        /// Увелечение масштаба
+        /// </summary>
+        /// <param name="scale"></param>
+        /// <returns></returns>
+        public double IncreaseSize(double scale)
+        {
+            return scale *= _scaleFactor;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Функция проверяет, вытянуто изображение вертикально, или горизонтально (при равенстве считаем изображение вертикальным)
+        /// </summary>
+        /// <param name="w">Ширина изображения</param>
+        /// <param name="h">Высота изображения</param>
+        /// <returns>Если истина, то изображение горизонтальное, если ложь - вертикальное</returns>
         public bool CompareSides(double w, double h)
         {
             var ar = Width / Height;
             return w > h * ar;
         }
 
-        bool panning = false;
-        Point lastPoint;
+        /// <summary>
+        /// Загружаем список файлов (с которыми работает программа) из текущей директории
+        /// </summary>
+        void GetFilesList()
+        {
+            var filters = "*.png|*.jpg|*.jpeg|*.gif|*.bmp|*.tif";
+            var path = System.IO.Path.GetDirectoryName(canvas.CurrentPath);
+            _filenames = filters.Split('|')
+                .SelectMany(f => Directory.GetFiles(path, f, SearchOption.TopDirectoryOnly))
+                .ToList();
+            _currentFileIndex = _filenames.IndexOf(canvas.CurrentPath);
+        }
+
+        #region Взаимодействие с пользователем
+
+        private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            Scale(e.Delta, e.GetPosition(canvas));
+        }
         
         private void imageField_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            panning = false;
+            _panning = false;
         }
 
         private void imageField_MouseMove(object sender, MouseEventArgs e)
         {
-            if (panning)
+            if (_panning)
             {
-                timeToWait = timeToWaitPreset;
+                _timeToWait = _timeToWaitPreset;
                 var newPoint = e.GetPosition(this);
-                var diff = lastPoint - newPoint;
-                lastPoint = newPoint;
+                var diff = _lastPoint - newPoint;
+                _lastPoint = newPoint;
 
                 canvas.ImgLeft -= diff.X;
                 canvas.ImgTop -= diff.Y;
@@ -191,12 +262,8 @@ namespace LightImageViewer
 
         private void imageField_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            panning = true;
-            lastPoint = e.GetPosition(this);
-        }
-
-        private void imageField_MouseLeave(object sender, MouseEventArgs e)
-        {
+            _panning = true;
+            _lastPoint = e.GetPosition(this);
         }
 
         private void MyWindow_KeyDown(object sender, KeyEventArgs e)
@@ -207,46 +274,30 @@ namespace LightImageViewer
             switch (e.Key)
             {
                 case Key.Down:
+                    // уменьшение масштаба
                     Scale(-1, centerPoint);
                     break;
                 case Key.Up:
+                    //величение масштаба
                     Scale(1, centerPoint);
                     break;
                 case Key.Left:
+                    // предыдущая картинка
+                    // список файлов получается ещё раз на случай изменения содержимого рабочего папки
                     GetFilesList();
-                    if (currentFileIndex == 0) break;
-                    currentFileIndex--;
-                    canvas.CurrentPath = filenames[currentFileIndex];
-                    text.Text = string.Format("{0}: {1}", currentFileIndex, canvas.CurrentPath);
+                    if (_currentFileIndex == 0) _currentFileIndex = 1;
+                    canvas.CurrentPath = _filenames[--_currentFileIndex];
                     break;
                 case Key.Right:
+                    // следующая картинка
+                    // список файлов получается ещё раз на случай изменения содержимого рабочего папки
                     GetFilesList();
-                    if (currentFileIndex == filenames.Count - 1) break;
-                    currentFileIndex++;
-                    canvas.CurrentPath = filenames[currentFileIndex];
-                    text.Text = string.Format("{0}: {1}", currentFileIndex, canvas.CurrentPath);
+                    if (_currentFileIndex >= _filenames.Count - 1) _currentFileIndex = _filenames.Count - 2;
+                    canvas.CurrentPath = _filenames[++_currentFileIndex];
                     break;
-            }
-            canvas.InvalidateVisual();
-        }
-
-        void GetFilesList()
-        {
-            var path = System.IO.Path.GetDirectoryName(canvas.CurrentPath);
-            filenames = GetFiles(path, "*.png|*.jpg|*.jpeg|*.gif|*.bmp|*.tif", SearchOption.TopDirectoryOnly);
-            for (var i = 0; i < filenames.Count; i++)
-            {
-                if (string.Equals(filenames[i], canvas.CurrentPath))
-                {
-                    currentFileIndex = i;
-                    break;
-                }
             }
         }
 
-        private static List<string> GetFiles(string sourceFolder, string filters, SearchOption searchOption)
-        {
-            return filters.Split('|').SelectMany(filter => Directory.GetFiles(sourceFolder, filter, searchOption)).ToList();
-        }
+        #endregion
     }
 }
--- LightImageViewer/MyCanvas.cs
+++ LightImageViewer/MyCanvas.cs
@@ -8,93 +8,127 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using WpfAnimatedGif;
 
 namespace LightImageViewer
 {
     public class MyCanvas : Canvas
     {
+        /// <summary>
+        /// Путь до текущего изображения
+        /// </summary>
         private string _currentPath;
-        private Image img;
+        private Image _img;
 
+        private Uri _uri;
+
+        private int _hCount = 0;
+        private int _wCount = 0;
+        private int _bmpHeight = 0;
+        private int _bmpWidth = 0;
+        private bool _widthBigger = false;
+        private double _aspect = 1;
+
+        /// <summary>
+        /// Путь до текущего изображения. Задание пути инициирует создание нового изображения,
+        /// загружаемого по указанному пути (задаются новые размеры и положение изображения).
+        /// При повторном задании того же пути, который используется в данный момент, 
+        /// ничего не будет происходить.
+        /// </summary>
         public string CurrentPath
         {
             get
-            { 
-                return _currentPath; 
+            {
+                return _currentPath;
             }
             set
             {
+                // если путь тот же, то игнорируем
+                if (string.Equals(_currentPath, value)) return;
+
                 _currentPath = value;
                 GetUri();
                 if ((_currentPath.Split('.').Last() as String).ToLower() == "gif")
-                    img = new AnimatedImage();
+                    _img = new AnimatedImage();
                 else
-                    img = new Image();
-                img.Stretch = Stretch.Fill;
+                    _img = new Image();
+                _img.Stretch = Stretch.Fill;
 
                 GetBmpParameters();
 
-                CurrentImage = img;
+                CurrentImage = _img;
                 var usedSize = 0d;
-                if (widthBigger)
+                if (_widthBigger)
                 {
-                    usedSize = Math.Min(ActualWidth, bmpWidth);
-                    img.Width = usedSize;
-                    img.Height = usedSize / aspect;
-                    ImgTop = ActualHeight / 2d - img.Height / 2d;
+                    usedSize = Math.Min(ActualWidth, _bmpWidth);
+                    _img.Width = usedSize;
+                    _img.Height = usedSize / _aspect;
+                    ImgTop = ActualHeight / 2d - _img.Height / 2d;
                     ImgLeft = ActualWidth / 2d - usedSize / 2d;
                 }
                 else
                 {
-                    usedSize = Math.Min(ActualHeight, bmpHeight);
-                    img.Height = usedSize;
-                    img.Width = usedSize * aspect;
+                    usedSize = Math.Min(ActualHeight, _bmpHeight);
+                    _img.Height = usedSize;
+                    _img.Width = usedSize * _aspect;
                     ImgTop = ActualHeight / 2d - usedSize / 2d;
-                    ImgLeft = ActualWidth / 2d - img.Width / 2d;
+                    ImgLeft = ActualWidth / 2d - _img.Width / 2d;
                 }
-                CurrentImage = img;
+                CurrentImage = _img;
                 UpdateImageSource();
                 Children.Clear();
                 GC.Collect();
-                Children.Add(img);
+                Children.Add(_img);
             }
         }
 
-        private Uri _uri;
-
+        /// <summary>
+        /// Текущий отрисовываемый объект Image
+        /// </summary>
         public Image CurrentImage { get; set; }
 
+        /// <summary>
+        /// Горизонтальное положение изображения
+        /// </summary>
         public double ImgLeft { get; set; }
 
+        /// <summary>
+        /// Вертикальное положение изображения
+        /// </summary>
         public double ImgTop { get; set; }
 
+        /// <summary>
+        /// Перекэширование изображения. Необходимо для подзагрузки новой копии изображения,
+        /// плотность пикселей которого будет совпадать с плотностью пикселей экрана. Таким образом,
+        /// будет загружено минимально возможное изображение, которое не будет иметь размытость.
+        /// </summary>
+        /// <param name="width">Ширина изображения на экране</param>
+        /// <param name="height">Высота изображения на экране</param>
+        /// <returns></returns>
         public BitmapImage PrecacheBmp(int width, int height)
         {
             GetUri();
             var bmp = new BitmapImage();
             bmp.BeginInit();
             bmp.CacheOption = BitmapCacheOption.None;
-            if (widthBigger)
-                bmp.DecodePixelWidth = Math.Min(width, bmpWidth);
+            // уловие, в зависимости от того, горизонтальное изображение или вертикальное,
+            // указывает битмапу, каков требуемый размер изображения в пикселях. Задаётся только
+            // одно измерение, второе будет сформировано автоматически в соответствии
+            // с соотношением сторон изображения. "Загружаемый размер" не должен превышать
+            // настоящий размер изображения
+            if (_widthBigger)
+                bmp.DecodePixelWidth = Math.Min(width, _bmpWidth);
             else
-                bmp.DecodePixelHeight = Math.Min(height, bmpHeight);
+                bmp.DecodePixelHeight = Math.Min(height, _bmpHeight);
             bmp.UriSource = _uri;
-            //var leftCrop = (int)Math.Max(-ImgLeft, 0);
-            //var topCrop = (int)Math.Max(-ImgTop, 0);
-            //var widthCrop = (int)Math.Min(-ImgLeft + ActualWidth, bmpWidth) - leftCrop;
-            //var heightCrop = (int)Math.Min(-ImgTop + ActualHeight, bmpHeight) - topCrop;
-            //bmp.SourceRect = new Int32Rect(leftCrop, topCrop, widthCrop, heightCrop);
             bmp.EndInit();
+            InvalidateVisual();
             return bmp;
         }
 
-        int hCount = 0;
-        int wCount = 0;
-        int bmpHeight = 0;
-        int bmpWidth = 0;
-        bool widthBigger = false;
-        double aspect = 1;
+        /// <summary>
+        /// Получение характеристик текущего изображения. Нужно для определения его реального размера в пикселях,
+        /// соотношения сторон и определения, вертикальное изображение или горизонтальное
+        /// </summary>
         private void GetBmpParameters()
         {
             GetUri();
@@ -103,24 +137,17 @@ namespace LightImageViewer
             bmp.CacheOption = BitmapCacheOption.None;
             bmp.UriSource = _uri;
             bmp.EndInit();
-            bmpHeight = bmp.PixelHeight;
-            bmpWidth = bmp.PixelWidth;
-            if (bmpHeight > ActualHeight)
-                hCount = (int)Math.Ceiling(bmpHeight / ActualHeight);
-            if (bmpWidth > ActualWidth)
-                wCount = (int)Math.Ceiling(bmpWidth / ActualWidth);
-            aspect = (double)bmpWidth / (double)bmpHeight;
+            _bmpHeight = bmp.PixelHeight;
+            _bmpWidth = bmp.PixelWidth;
+            if (_bmpHeight > ActualHeight)
+                _hCount = (int)Math.Ceiling(_bmpHeight / ActualHeight);
+            if (_bmpWidth > ActualWidth)
+                _wCount = (int)Math.Ceiling(_bmpWidth / ActualWidth);
+            _aspect = (double)_bmpWidth / (double)_bmpHeight;
 
-            widthBigger = false;
-            if (ActualWidth / ActualHeight < aspect)
-                widthBigger = true;
-        }
-
-        public bool CanMakeBigger(int width, int height)
-        {
-            return true;
-            var bmp = new BitmapImage(_uri);
-            return bmp.Height > height && bmp.Width > width;
+            _widthBigger = false;
+            if (ActualWidth / ActualHeight < _aspect)
+                _widthBigger = true;
         }
 
         public void GetUri()
@@ -139,13 +166,9 @@ namespace LightImageViewer
         {
             GetUri();
             if ((_currentPath.Split('.').Last() as String).ToLower() == "gif")
-            {
-                //ImageBehavior.SetAnimatedSource(img, PrecacheBmp((int)img.Width, (int)img.Height));
-                (img as AnimatedImage).Source = PrecacheBmp((int)img.Width, (int)img.Height);
-                //(img as GifImage).GifSource = _uri;
-            }
+                (_img as AnimatedImage).Source = PrecacheBmp((int)_img.Width, (int)_img.Height);
             else
-                img.Source = PrecacheBmp((int)img.Width, (int)img.Height);
+                _img.Source = PrecacheBmp((int)_img.Width, (int)_img.Height);
         }
         
         protected override void OnRender(DrawingContext dc)
